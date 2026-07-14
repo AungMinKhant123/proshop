2001065923f79723b54dd3249b05ce66f2bc49b9
Aung Min Khant <89652503+AungMinKhant123@users.noreply.github.com>
2026-07-14 22:20:12 +0630

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
